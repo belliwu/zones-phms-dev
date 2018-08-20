e1050b05a86f0a5b499dc1de770ef70c762503c5
ZONES <belliwu@iii.org.tw>
2018-08-20 15:03:15 +0800

    Dynamic update login or signup message

--- src/views/layout/HeaderNavs.vue
+++ src/views/layout/HeaderNavs.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="myNavbar ml-auto">
+  <div class="headerNavs ml-auto">
+
     <ul class="navbar-nav">
-      <li class="nav-item">
+      <li class="nav-item my-auto text-white" v-if="email">{{email}}</li>
+
+      <li class="nav-item" v-if="email">
         <a href="#" class="nav-link">
           <i class="fas fa-envelope"></i> 5</a>
       </li>
-      <li class="nav-item">
+
+      <li class="nav-item" v-if="email">
         <a href="#" class="nav-link">
           <i class="fas fa-bell"></i> 3</a>
       </li>
@@ -13,26 +17,25 @@
       <li class="nav-item dropdown">
         <a href="#" id="dd_user" class="nav-link dropdown-toggle" data-toggle="dropdown">
           <i class="fas fa-user-circle fa-lg"></i>
-          Belli Wu
         </a>
 
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd_user">
-          <router-link class="dropdown-item" to="/myInfo">
+          <router-link class="dropdown-item" to="/myInfo" v-if="email">
             <i class="fas fa-user"></i>
             個人資料
           </router-link>
 
-          <router-link class="dropdown-item" to="/signup">
+          <router-link class="dropdown-item" to="/signup" v-if="!email">
             <i class="fas fa-user-plus"></i>
             註冊
           </router-link>
 
-          <router-link class="dropdown-item" to="/login">
+          <router-link class="dropdown-item" to="/login" v-if="!email">
             <i class="fas fa-sign-in-alt"></i>
             簽到
           </router-link>
 
-          <router-link class="dropdown-item" to="/logout">
+          <router-link class="dropdown-item" to="/logout" v-if="email">
             <i class="fas fa-sign-out-alt"></i>
             登出
           </router-link>
@@ -46,7 +49,15 @@
 
 <script>
 export default {
-  name: "headerNavs"
+  name: "headerNavs",
+  computed: {
+    email() {
+      return !this.$store.getters.user ? false : this.$store.getters.user.email;
+    }
+  },
+  created() {
+    this.$store.dispatch("fetchUser");
+  }
 };
 </script>
 
